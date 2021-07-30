d075b4d729fcd243acbd2e4969de3c6e1a8aca39
gritsiem <46785182+gritsiem@users.noreply.github.com>
2021-07-31 01:37:59 +0530

    Update guestbook.js
    Added API base path

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://9d9156f8.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
